d3c052910f72fe0b1876185a28001244d11b313b
codeman1221 <133810552+codeman1221@users.noreply.github.com>
2023-12-12 13:53:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+# Load existing logbook data or create an empty DataFrame
+logbook_data = pd.DataFrame(columns=['Date', 'Route', 'Grade', 'Notes'])
+
+# Function to add a climb to the logbook
+def add_climb(date, route, grade, notes):
+    global logbook_data
+    logbook_data = logbook_data.append({'Date': date, 'Route': route, 'Grade': grade, 'Notes': notes}, ignore_index=True)
+
+# Streamlit UI
+st.title('Climbing Logbook App')
+
+# Sidebar for adding climbs
+st.sidebar.header('Add Climb')
+climb_date = st.sidebar.date_input('Date', datetime.today())
+climb_route = st.sidebar.text_input('Route', '')
+climb_grade = st.sidebar.text_input('Grade', '')
+climb_notes = st.sidebar.text_area('Notes', '')
+
+if st.sidebar.button('Add Climb'):
+    add_climb(climb_date, climb_route, climb_grade, climb_notes)
+    st.sidebar.success('Climb added to logbook!')
+
+# Display the logbook
+st.header('Climbing Logbook')
+st.table(logbook_data.sort_values(by='Date', ascending=False).reset_index(drop=True))
 
